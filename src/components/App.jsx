e5be915132eeca7dbf3118e030270337dffde4f6
Thomas Puppe <thomas.puppe@zeit.de>
2017-12-15 15:45:03 +0100

    introduce AudioSettings component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { TeaserList } from 'app/components';
+import { TeaserList, AudioSettings } from 'app/components';
 import logo from 'app/svg/logo.svg';
 
 const App = () => (
@@ -8,8 +8,10 @@ const App = () => (
         <header className="header">
             <svg className="header_logo"><use xlinkHref={logo.url} /></svg>
         </header>
+        <AudioSettings />
         <TeaserList headline="Audio Explorer" />
     </div>
+
 );
 
 export default App;
