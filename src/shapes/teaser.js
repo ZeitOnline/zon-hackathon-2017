bdf7af1c3779dc6eace4c224f4a00c8a97a56f7d
Nico Bruenjes <nico.bruenjes@gmail.com>
2017-12-15 15:01:18 +0100

    Add author and body

--- src/shapes/teaser.js
+++ src/shapes/teaser.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
 export default {
+    author: PropTypes.arrayOf(PropTypes.string),
+    body: PropTypes.string.isRequired,
     href: PropTypes.string.isRequired,
     release_date: PropTypes.string.isRequired,
     supertitle: PropTypes.string.isRequired,
