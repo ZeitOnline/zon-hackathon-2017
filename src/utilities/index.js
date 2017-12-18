dcd30f5b6cc74d1ffd8af8f72a032a1796dca859
Moritz Stoltenburg <slomo@slomo.de>
2017-12-18 22:01:20 +0100

    [Utilities] Rename file

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,3 @@
 export fetchData from './fetchData';
-export { defaultAudioSettings, getVoices, getDefaultName } from './SpeechSynth';
+export { defaultAudioSettings, getVoices, getDefaultName } from './webSpeech';
 export { distanceToNow, formatDate } from './time';
