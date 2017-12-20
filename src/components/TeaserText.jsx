8a794f97a28960941ea7fd0e1528a14df6d249c6
Moritz Stoltenburg <slomo@slomo.de>
2017-12-20 10:26:59 +0100

    [TeaserText] Scroll mark into view if needed

--- src/components/TeaserText.jsx
+++ src/components/TeaserText.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-const TeaserText = ({ text, charIndex, hidden }) => {
-    const end = text.replace(/\s/g, ' ').indexOf(' ', charIndex);
+export default class TeaserText extends PureComponent {
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        charIndex: PropTypes.number.isRequired,
+        hidden: PropTypes.bool.isRequired,
+    };
 
-    return (
-        <p className="teaser__text" hidden={hidden}>
-            {text.slice(0, charIndex)}
-            <mark>{text.slice(charIndex, end)}</mark>
-            {text.slice(end)}
-        </p>
-    );
-};
+    componentDidUpdate() {
+        if (!this.props.hidden) {
+            if ('scrollIntoViewIfNeeded' in this.mark) {
+                this.mark.scrollIntoViewIfNeeded(true);
+            } else if ('scrollIntoView' in this.mark) {
+                // waiting for FF 58
+                // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1389274
+                // this.mark.scrollIntoView({
+                //     behavior: 'smooth',
+                //     block: 'nearest',
+                // });
+            }
+        }
+    }
 
-TeaserText.propTypes = {
-    text: PropTypes.string.isRequired,
-    charIndex: PropTypes.number.isRequired,
-    hidden: PropTypes.bool.isRequired,
-};
+    render() {
+        const { text, charIndex, hidden } = this.props;
+        const end = text.replace(/\s/g, ' ').indexOf(' ', charIndex);
 
-export default TeaserText;
+        return (
+            <p className="teaser__text" hidden={hidden}>
+                {text.slice(0, charIndex)}
+                <mark ref={(element) => { this.mark = element; }}>
+                    {text.slice(charIndex, end)}
+                </mark>
+                {text.slice(end)}
+            </p>
+        );
+    }
+}
