3064a6bafe14fdcb1aa2b0c861d3f7fd88789161
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2017-12-28 13:59:26 +0100

    [AudioSetting] Adding tick marks
    …after reading https://css-tricks.com/sliding-nightmare-understanding-range-input/

--- src/components/AudioSetting.jsx
+++ src/components/AudioSetting.jsx
@@ -1,36 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const AudioSetting = ({ name, value, min, max, step, onChange }) => (
-    <label htmlFor={name} className="settings__label">
-        <span>{ name }</span>
-        <span>{ value }</span>
-        <input
-            id={name}
-            name={name}
-            type="range"
-            value={value}
-            min={min}
-            max={max}
-            step={step}
-            onChange={onChange}
-        />
-    </label>
-);
+export default class AudioSetting extends Component {
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+        min: PropTypes.number,
+        max: PropTypes.number,
+        step: PropTypes.number,
+        onChange: PropTypes.func.isRequired,
+    };
 
-AudioSetting.propTypes = {
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    min: PropTypes.number,
-    max: PropTypes.number,
-    step: PropTypes.number,
-    onChange: PropTypes.func.isRequired,
-};
+    static defaultProps = {
+        min: 0,
+        max: 1,
+        step: 0.1,
+    };
 
-AudioSetting.defaultProps = {
-    min: 0,
-    max: 1,
-    step: 0.1,
-};
+    render() {
+        const { name, value, min, max, step, onChange } = this.props;
+        const listId = `datalist-${name}`;
 
-export default AudioSetting;
+        return (
+            <label htmlFor={name} className="settings__label">
+                <span>{ name }</span>
+                <output htmlFor={name}>{ value }</output>
+                <input
+                    id={name}
+                    name={name}
+                    type="range"
+                    value={value}
+                    min={min}
+                    max={max}
+                    step={step}
+                    onChange={onChange}
+                    list={listId}
+                />
+                {this.renderTickMarks(listId)}
+            </label>
+        );
+    }
+
+    renderTickMarks(listId) {
+        const { min, max, step } = this.props;
+        const list = [];
+        // @see http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
+        const factor = 1 / step;
+        let tick = min;
+
+        while (tick <= max) {
+            list.push(tick);
+            tick = Math.round((tick + step) * factor) / factor;
+        }
+
+        return (
+            <datalist id={listId}>
+                {list.map(value => (
+                    <option key={value} label={value}>{value}</option>
+                ))}
+            </datalist>
+        );
+    }
+}
