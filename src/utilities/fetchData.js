df34a8824fb0c7872d6ef25670e37951966d265f
Valentin von Guttenberg <greengiraffe@posteo.de>
2018-02-15 17:44:09 +0100

    Add test JSON for fetching

--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -7,10 +7,58 @@ const headers = new Headers({
 
 
 const fetchData = async () => {
-    const fields = Object.keys(TEASER).join(',');
-    const uri = `http://api.zeit.de/product/zede?fields=${fields}`;
-    const response = await fetch(uri, { headers });
-    return response.json();
+    if (!IS_PRODUCTION) {
+        const fields = Object.keys(TEASER).join(',');
+        const uri = `http://api.zeit.de/product/zede?fields=${fields}`;
+        const response = await fetch(uri, { headers });
+        return response.json();
+    }
+    return testJSON;
+};
+
+const testJSON = {
+    matches: [
+        {
+            body: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            uuid: 'test1',
+            title: 'Testtitel',
+            href: 'http://www.zeit.de/kultur/film/2018-02/dieter-wedel-sexuelle-uebergriffe-zdf',
+            release_date: '2018-02-15T12:20:19Z',
+            supertitle: 'Testkicker',
+            teaser_text: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            teaser_title: 'Teasertitel',
+        },
+        {
+            body: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            uuid: 'test2',
+            title: 'Testtitel',
+            href: 'http://www.zeit.de/kultur/film/2018-02/dieter-wedel-sexuelle-uebergriffe-zdf',
+            release_date: '2018-02-15T12:20:19Z',
+            supertitle: 'Testkicker',
+            teaser_text: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            teaser_title: 'Teasertitel',
+        },
+        {
+            body: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            uuid: 'test3',
+            title: 'Testtitel',
+            href: 'http://www.zeit.de/kultur/film/2018-02/dieter-wedel-sexuelle-uebergriffe-zdf',
+            release_date: '2018-02-15T12:20:19Z',
+            supertitle: 'Testkicker',
+            teaser_text: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            teaser_title: 'Teasertitel',
+        },
+        {
+            body: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            uuid: 'test4',
+            title: 'Testtitel',
+            href: 'http://www.zeit.de/kultur/film/2018-02/dieter-wedel-sexuelle-uebergriffe-zdf',
+            release_date: '2018-02-15T12:20:19Z',
+            supertitle: 'Testkicker',
+            teaser_text: 'Ein zwei drei vier fünf sechs sieben acht neun zehn',
+            teaser_title: 'Teasertitel',
+        },
+    ],
 };
 
 export default fetchData;
