1668e05bc5e6d5aa7e760574f991db434867ceea
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2018-04-05 14:41:24 +0200

    Simplify component

--- src/components/TimeEstimate.jsx
+++ src/components/TimeEstimate.jsx
@@ -1,41 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { formatDate, estimatedReadingTime } from 'app/utilities';
 
-class TimeEstimate extends Component {
-    static propTypes = {
-        wordCount: PropTypes.number.isRequired,
-        speechRate: PropTypes.number.isRequired,
-    };
+const TimeEstimate = ({ estimatedTime }) => (
+    <span className="time-estimate">{formatDate(estimatedTime, 'mm:ss')}</span>
+);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            estimatedTime: estimatedReadingTime(
-                props.wordCount,
-                props.speechRate,
-            ),
-        };
-    }
+TimeEstimate.propTypes = {
+    estimatedTime: PropTypes.number.isRequired,
+};
 
-    componentWillReceiveProps({ wordCount, speechRate }) {
-        this.estimatedTime = estimatedReadingTime(wordCount, speechRate);
-        this.setState({
-            estimatedTime: this.estimatedTime,
-        });
-    }
-
-    render() {
-        return (
-            <span className="time-estimate">{formatDate(this.state.estimatedTime, 'mm:ss')}</span>
-        );
-    }
-}
-
-const mapStateToProps = ({ audioSettings }) => ({
-    speechRate: audioSettings.rate,
+const mapStateToProps = ({ audioSettings }, { wordCount }) => ({
+    estimatedTime: estimatedReadingTime(wordCount, audioSettings.rate),
 });
 
 export default connect(mapStateToProps)(TimeEstimate);
